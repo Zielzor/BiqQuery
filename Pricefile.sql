548dd5de64994de1e84c294874525a2ee9e5d5b8
Lukasz <zielzor@gmail.com>
2019-10-25 14:12:24 +0200

    Add files via upload

--- Pricefile.sql
+++ Pricefile.sql
@@ -1,15 +1,24 @@
-SELECT 
-distinct( productNo_key ),
-productDesc,
-salesPrice as Sprzedaz_Brutto_PLN,
-round((salesPrice / 1.23)) as Sprzedaz_Netto_PLN,
-unitSalesPrice
-priceUnit,
-salesUnit,
-productTaxClassification,
-flagPriceControl
-FROM `conrad-cbdp-prod-core.de_conrad_dwh1000_dwh_DimProductSalesOrg.DimProductSalesOrg` 
-WHERE salesOrg_key='5810' 
-AND  salesPrice>0 
-AND statusWebshop ='C'
-ORDER BY salesPrice  DESC
+SELECT distinct
+b.productNo_key,
+b.productDesc,
+b.salesPrice as Sprzedaz_Brutto_PLN,
+round((b.salesPrice / 1.23)) as Sprzedaz_Netto_PLN,
+b.unitSalesPrice,
+b.priceUnit, 
+b.salesUnit,
+b.productTaxClassification,
+b.flagPriceControl,
+a.productDescEng,  
+a.statusCrossSite , -- in COBIS_DWH D_ARTIKEL.mstae
+a.statusCrossDistrChain , -- in COBIS_DWH D_ARTIKEL.mstav
+a.statusDistriChainSpecific, -- in COBIS_DWH D_ARTIKEL.vmsta
+b.salesOrg_key
+
+FROM `conrad-cbdp-prod-core.de_conrad_dwh1000_dwh_DimProduct.DimProduct` a
+INNER JOIN `conrad-cbdp-prod-core.de_conrad_dwh1000_dwh_DimProductSalesOrg.DimProductSalesOrg` b
+  ON a.productNo_key=b.productNo_key
+
+WHERE b.salesOrg_key="5810"
+AND b.statusWebshop ='C'
+
+ORDER BY b.productNo_key
